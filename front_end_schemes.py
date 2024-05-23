b2492bca06796cc4da24c546cfd830087bcf9e83
Edward Townsend <ejpt2@cam.ac.uk>
2024-05-23 22:35:45 +0100

    ADD: adding functions to make implementing front-end schemes faster

---
+++ front_end_schemes.py
@@ -0,0 +1,29 @@
+%matplotlib widget
+import warnings
+import inspect
+import matplotlib.pyplot as plt
+import IPython.display
+import numpy as np
+from cued_sf2_lab.dct import colxfm, dct_ii, regroup
+from cued_sf2_lab.lbt import pot_ii
+from useful_functions import *
+
+def gen_lbt(X, block_size, s):
+    if block_size <= 0 or (block_size & (block_size - 1)) != 0:
+        return "Error the block size is not a power of two!"
+
+    C = dct_ii(block_size)
+    step_size = find_step_equal_rms(X, C, s)
+    Yq = gen_Y_quant_lbt(X, step_size C, s)
+    Yr = regroup(Yq, 4)
+    Yr_ent = dctbpp(Yr, 16)
+    comp_ratio =  X_quant_entropy(X) / Yr_ent
+    
+    Z = colxfm(colxfm(Yq.T, C.T).T, C.T)
+    Zp = Z.copy()
+    Pf, Pr = pot_ii(block_size, s)
+    t = np.s_[block_size//2:-block_size//2]
+    Zp[:,t] = colxfm(Zp[:,t].T, Pr.T).T
+    Zp[t,:] = colxfm(Zp[t,:], Pr.T)
+
+    return comp_ratio, Zp
---
+++ useful_functions.py
@@ -0,0 +1,84 @@
+%matplotlib widget
+import warnings
+import inspect
+import matplotlib.pyplot as plt
+import IPython.display
+import numpy as np
+from cued_sf2_lab.dct import colxfm, dct_ii
+from cued_sf2_lab.lbt import pot_ii
+from cued_sf2_lab.laplacian_pyramid import bpp, quantise
+
+def X_quant_entropy(X, step_size=17):
+    X_quant = quantise(X, 17)
+    return bpp(X_quant) * X_quant.size
+
+def energy(X):
+    return np.sum(X ** 2.0)
+
+# LBT functions
+def dctbpp(Yr, N):
+    m, n = Yr.shape
+    if m % N != 0 or n % N != 0:
+        raise ValueError('Height/width of Yr not multiple of N')
+    
+    entropy_sum = 0
+    for i in range(0, m, m//N):
+        for j in range(0, n, n//N):
+            sub_img = Yr[i:i+m//N, j:j+n//N]
+            sub_img_ent = bpp(sub_img) * sub_img.size
+            entropy_sum += sub_img_ent
+
+    return entropy_sum
+
+def gen_Y_quant_lbt(X, step_size, C, s):
+    N = C.shape[0]
+    Pf, Pr = pot_ii(N, s)
+    t = np.s_[N//2:-N//2]
+
+    Xp = X.copy()
+    Xp[t, :] = colxfm(Xp[t, :], Pf)
+    Xp[:, t] = colxfm(Xp[:, t].T, Pf).T
+
+    Y = colxfm(colxfm(Xp, C).T, C).T
+    Yq = quantise(Y, step_size)
+
+    return Yq
+
+def gen_Z_quant_lbt(X, step_size, C, s):
+    N = C.shape[0]
+    Pf, Pr = pot_ii(N, s)
+    t = np.s_[N//2:-N//2] 
+
+    Xp = X.copy()
+    Xp[t, :] = colxfm(Xp[t, :], Pf)
+    Xp[:, t] = colxfm(Xp[:, t].T, Pf).T
+
+    Y = colxfm(colxfm(Xp, C).T, C).T
+    Yq = quantise(Y, step_size)
+
+    Z = colxfm(colxfm(Yq.T, C.T).T, C.T)
+    Zp = Z.copy()
+    Zp[:, t] = colxfm(Zp[:, t].T, Pr.T).T
+    Zp[t, :] = colxfm(Zp[t, :], Pr.T)
+
+    return Zp
+
+def compute_err_lbt(X, step_size, C, s):
+    Zp = gen_Z_quant_lbt(X, step_size, C, s)
+    return np.std(X - Zp)
+
+def find_step_equal_rms(X, C, s):
+    target_err = np.std(X - quantise(X, 17))
+
+    # Binary search
+    low, high = 15, 30
+    while high - low > 0.1:
+        mid = (low + high) / 2
+        err = compute_err_lbt(X, mid, C, s)
+
+        if err < target_err:
+            low = mid
+        else:
+            high = mid
+
+    return (low + high) / 2
