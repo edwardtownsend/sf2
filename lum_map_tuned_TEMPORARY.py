6fad5418172136aa9d2ad964ab657ec8f3069cd4
Ultrasonix3 <99297759+Ultrasonix3@users.noreply.github.com>
2024-06-02 22:10:46 +0100

    Added rms error calculation and a 'final' compression function

--- lum_map_tuned_TEMPORARY.py
+++ lum_map_tuned_TEMPORARY.py
@@ -31,5 +31,32 @@ def tuneDCTmap(X, s=None):
         Zp[:, t] = colxfm(Zp[:, t].T, Pr.T).T
         Zp[t, :] = colxfm(Zp[t, :], Pr.T)
     score = visualerror(X, Zp)
+    rms_err = np.std(Z-X)
 
-    return Zp, calc_bits, score
+    return Zp, calc_bits, score, rms_err
+
+
+### Final image compression function? If DWT isnt working we can simplify it down to choose between LBT and DCT
+def image_compression(X):
+    Z1, B1, S1, e1 = tuneDCTmap(X, s=None)
+    Z2, B2, S2, e1 = tuneDCTmap(X, s=np.sqrt(2))  #We can choose an s value on the morning of?
+    """ potential line of code if DWT is working
+    Z3, B3, S3, e3 = LBTFUNCTION() """
+    if S1 > S2:
+        if S1 > S3:
+            print(f"Image using DCT method, {B1} bits, rms error of {e1} and SSIM score of {S1}")
+            fig, ax = plt.subplots()
+            plot_image(Z1, ax=ax)
+        elif S3 > S1:
+            print(f"Image using DCT method, {B3} bits, rms error of {e3} and SSIM score of {S3}")
+            fig, ax = plt.subplots()
+            plot_image(Z3, ax=ax)
+    elif S2 > S1:
+        if S2 > S3:
+            print(f"Image using DCT method, {B2} bits, rms error of {e2} and SSIM score of {S2}")
+            fig, ax = plt.subplots()
+            plot_image(Z2, ax=ax)        
+        elif S3 > S2:
+            print(f"Image using DCT method, {B3} bits, rms error of {e3} and SSIM score of {S3}")
+            fig, ax = plt.subplots()
+            plot_image(Z3, ax=ax)
